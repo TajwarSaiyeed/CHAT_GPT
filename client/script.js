528f84aef35bafea552358dad32c255d4b0401ea
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-19 08:05:31 +0600

    chat Stripe added

--- client/script.js
+++ client/script.js
@@ -36,3 +36,18 @@ function generateUniqueId() {
 
   return `id-${timestamp}-${hexaDecimalString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+      <div class="wrapper ${isAi && "ai"}">
+        <div class="chat">
+          <div class="profile">
+            <img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}" />
+          </div>
+          <div class="message" id="${uniqueId}">
+           ${value}
+          </div>
+        </div>
+      </div>
+    `;
+}
